dc0bab8e3786eb0a6b0370b4e1451cb158023136
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-05-25 18:00:06 +0200

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
 	"os"
@@ -14,11 +15,26 @@ func main() {
 		errorLogger.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
 	conn, err := l.Accept()
 	if err != nil {
 		errorLogger.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
 
-	conn.Write([]byte("+PONG\r\n"))
+	for {
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				errorLogger.Println("EOF: Remote client closed connection")
+			} else {
+				errorLogger.Println("Error reading from connection: ", err.Error())
+			}
+			os.Exit(1)
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
