74a06a577a90142b24d5dd637624ef16e0287c51
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-05-25 17:39:10 +0200

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
+	errorLogger := log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		errorLogger.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		errorLogger.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	conn.Write([]byte("+PONG\r\n"))
 }
