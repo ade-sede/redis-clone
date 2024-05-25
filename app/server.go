bd356d7f0da41802d2f4dd93752f75f1a5de8e2b
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-05-25 18:23:47 +0200

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
 )
+
+func handleConnection(conn net.Conn, errorChannel chan error) {
+	for {
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				errorChannel <- fmt.Errorf("EOF: Remote client closed connection")
+			} else {
+				errorChannel <- fmt.Errorf("Error reading from connection: %s", err.Error())
+			}
+			conn.Close()
+			return
+		}
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			errorChannel <- fmt.Errorf("Error writing to connection: %s", err.Error())
+			return
+		}
+	}
+}
 
 func main() {
 	errorLogger := log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
@@ -15,26 +38,24 @@ func main() {
 		errorLogger.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		errorLogger.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer conn.Close()
+	errorChannel := make(chan error, 100)
+
+	go func() {
+		for err := range errorChannel {
+			errorLogger.Println(err)
+		}
+	}()
 
 	for {
-		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+		conn, err := l.Accept()
 		if err != nil {
-			if err == io.EOF {
-				errorLogger.Println("EOF: Remote client closed connection")
-			} else {
-				errorLogger.Println("Error reading from connection: ", err.Error())
-			}
-			os.Exit(1)
+			errorLogger.Println("Error accepting connection: ", err.Error())
+			continue
 		}
 
-		conn.Write([]byte("+PONG\r\n"))
+		go handleConnection(conn, errorChannel)
 	}
+
 }
