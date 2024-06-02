ef253e22a87119d41be9f1935cdc9fe1f82c37b9
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-06-02 09:26:24 +0200

    refactor: replication

--- app/replication.go
+++ app/replication.go
@@ -59,16 +59,13 @@ var replicationInfo struct {
 // The simplest pattern to implement is FULLRESYNC.
 // As an aswer to `PSYNC` master answers with `FULLRESYNC` and proceeds to send
 // the whole RDB file to the slave.
-
-func initReplication(slaveListeningPort int) (*net.Conn, error) {
+func initReplication(listeningPort int) (*net.Conn, error) {
 	if replicationInfo.replicaof == "" {
 		replicationInfo.masterReplId = generateReplId()
 		replicationInfo.masterReplOffset = 0
 
 		return nil, nil
 	}
-
-	buf := make([]byte, 4096)
 
 	replicationInfo.masterReplId = "?"
 	replicationInfo.masterReplOffset = -1
@@ -97,126 +94,107 @@ func initReplication(slaveListeningPort int) (*net.Conn, error) {
 
 	replicationInfo.masterConnection = &conn
 
-	// TODO encapsulate handshake + response expectation
-
-	pingCommand := encodeStringArray([]string{"PING"})
-	_, err = conn.Write(pingCommand)
+	err = handshake(conn, listeningPort)
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = conn.Read(buf)
+	return &conn, nil
+}
+
+func handshake(conn net.Conn, listeningPort int) error {
+	var err error
+
+	_, err = sendMsg(conn, []string{"PING"}, "PONG")
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	offset := 0
-	query, err := parseResp(buf, &offset)
-	if err != nil {
-		return nil, err
-	}
-
-	pong, err := query.asString()
-	if pong != "PONG" {
-		return nil, fmt.Errorf("Expected PONG as an answer to PING")
-	}
-
-	replConfCommand := encodeStringArray([]string{
+	_, err = sendMsg(conn, []string{
 		"REPLCONF",
 		"listening-port",
-		strconv.Itoa(slaveListeningPort),
-	})
-	_, err = conn.Write(replConfCommand)
+		strconv.Itoa(listeningPort),
+	}, "OK")
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	_, err = conn.Read(buf)
-	if err != nil {
-		return nil, err
-	}
-
-	offset = 0
-	query, err = parseResp(buf, &offset)
-	if err != nil {
-		return nil, err
-	}
-
-	ok, err := query.asString()
-	if ok != "OK" {
-		return nil, fmt.Errorf("Expected OK as an answer to REPLCONF")
-	}
-
-	replConfCommand = encodeStringArray([]string{
+	_, err = sendMsg(conn, []string{
 		"REPLCONF",
 		"capa",
 		"psync2",
 		"capa",
 		"eof",
-	})
-	_, err = conn.Write(replConfCommand)
+	}, "OK")
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	_, err = conn.Read(buf)
-	if err != nil {
-		return nil, err
-	}
-
-	offset = 0
-	query, err = parseResp(buf, &offset)
-	if err != nil {
-		return nil, err
-	}
-
-	ok, err = query.asString()
-	if ok != "OK" {
-		return nil, fmt.Errorf("Expected OK as an answer to REPLCONF")
-	}
-
-	psyncCommand := encodeStringArray([]string{
+	response, err := sendMsg(conn, []string{
 		"PSYNC",
 		replicationInfo.masterReplId,
 		strconv.Itoa(replicationInfo.masterReplOffset),
-	})
-
-	_, err = conn.Write(psyncCommand)
+	}, "")
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	_, err = conn.Read(buf)
-	if err != nil {
-		return nil, err
-	}
-
-	offset = 0
-	query, err = parseResp(buf, &offset)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := query.asString()
-	array := strings.Fields(s)
+	array := strings.Fields(response)
 	if len(array) != 3 || array[0] != "FULLRESYNC" {
-		return nil, fmt.Errorf("Expected reponse in the format: FULLRESYNC <REPL_ID> <REPL_OFFSET>")
+		return fmt.Errorf("Expected reponse in the format: FULLRESYNC <REPL_ID> <REPL_OFFSET>")
 	}
 
 	_, err = hex.DecodeString(array[1])
 	if err != nil || len(array[1]) != 40 {
-		return nil, fmt.Errorf("Expected repl ID to be a 40 digit hex string")
+		return fmt.Errorf("Expected repl ID to be a 40 digit hex string")
 	}
 
 	replicationInfo.masterReplId = array[1]
 
 	replOffset, err := strconv.Atoi(array[2])
 	if err != nil || replOffset < 0 {
-		return nil, fmt.Errorf("Expected repl offset to be a positive integer")
+		return fmt.Errorf("Expected repl offset to be a positive integer")
 
 	}
 
-	replicationInfo.masterReplOffset = offset
+	replicationInfo.masterReplOffset = replOffset
 
-	return &conn, nil
+	return nil
+}
+
+func sendMsg(conn net.Conn, command []string, expect string) (string, error) {
+	var response string
+	var offset int
+
+	buf := make([]byte, 4096)
+
+	commandArray := encodeStringArray(command)
+
+	_, err := conn.Write(commandArray)
+	if err != nil {
+		return "", err
+	}
+
+	_, err = conn.Read(buf)
+	if err != nil {
+		return "", err
+	}
+
+	query, err := parseResp(buf, &offset)
+	if err != nil {
+		return "", err
+	}
+
+	if expect != "" {
+		response, err = query.asString()
+		if err != nil {
+			return "", err
+		}
+
+		if response != expect {
+			return "", fmt.Errorf("Expected %s as an answer to PING, got %s", expect, response)
+		}
+	}
+
+	return response, nil
 }
