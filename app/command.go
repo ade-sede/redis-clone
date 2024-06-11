c606c1e864605586e041dfedc07c7cd5ece1d4a4
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-06-11 13:21:24 +0200

    feat: select

--- app/command.go
+++ app/command.go
@@ -18,9 +18,14 @@ const (
 	REPLCONF
 	REPLCONF_GETACK
 	WAIT
+	SELECT
 )
 
 const EMPTY_RDB_FILE = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+
+func selectFunc() []byte {
+	return []byte("+OK\r\n")
+}
 
 func ping() []byte {
 	return []byte("+PONG\r\n")
@@ -119,6 +124,11 @@ func execute(conn *connection, query *query) ([]byte, command, error) {
 	if strings.EqualFold(command, "WAIT") {
 		response := wait(args)
 		return response, WAIT, nil
+	}
+
+	if strings.EqualFold(command, "SELECT") {
+		response := selectFunc()
+		return response, SELECT, nil
 	}
 
 	return nil, UNKNOWN, fmt.Errorf("%w unknown command '%s'", ErrRespSimpleError, command)
